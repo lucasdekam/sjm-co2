333e0d864452f54b06ba3e86c23865d921462de9
Lucas <lucasdekam@gmail.com>
2024-12-04 12:55:47 +0100

    load appropriate modules

--- submit.sh
+++ submit.sh
@@ -4,5 +4,13 @@
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00:00
 
+module load 2023
+module load OpenMPI/4.1.5-GCC-12.3.0
+module load OpenBLAS/0.3.23-GCC-12.3.0
+module load libxc/6.2.2-GCC-12.3.0
+module load ScaLAPACK/2.2.0-gompi-2023a-fb
+module load FFTW.MPI/3.3.10-gompi-2023a
+module load Python/3.11.3-GCCcore-12.3.0
+
 export OMP_NUM_THREADS=1
 mpirun -np 64 gpaw python make-stepped-config.py
